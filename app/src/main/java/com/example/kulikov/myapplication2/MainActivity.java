1d26f53b1aeae619ca550118f2c69e4c10133a5b
Pavel Kulikov <kulikovpavel@gmail.com>
2016-07-23 10:02:17 +0300

    initial

---
+++ app/src/main/java/com/example/kulikov/myapplication2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.kulikov.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    List<Person> persons;
+
+    // This method creates an ArrayList that has three Person objects
+// Checkout the project associated with this tutorial on Github if
+// you want to use the same images.
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.mipmap.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.mipmap.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.mipmap.lillie));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        initializeData();
+
+        RVAdapter adapter = new RVAdapter(persons);
+        mRecyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/kulikov/myapplication2/Person.java
@@ -0,0 +1,19 @@
+package com.example.kulikov.myapplication2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kulikov on 23.07.16.
+ */
+class Person {
+    String name;
+    String age;
+    int photoId;
+
+    Person(String name, String age, int photoId) {
+        this.name = name;
+        this.age = age;
+        this.photoId = photoId;
+    }
+}
